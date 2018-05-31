39fb7c19ba6bf4e389854d899fc609a7c273f288
Mikhail <amjidkov@mail.ru>
2018-05-31 20:05:29 +0300

    add new $a

--- my_test.php
+++ my_test.php
@@ -27,6 +27,7 @@
    		{
             $kal[] = $value;
    		}
+        $a = 5;
         file_put_contents('test.json', json_encode($kal, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
         ?>
     </body>
