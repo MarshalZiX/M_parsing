a469fe29e704b51c80fb00b8cf57e23caa158d62
MarshalZiX <majidkov@mail.ru>
2018-05-31 19:58:58 +0300

    add test

---
+++ my_test.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>тест</title> 
+    </head>
+    <body>
+        <?php
+        include("simplehtmldom\simple_html_dom.php");
+    	function get_info($url)
+    	{
+    		foreach (file_get_html($url) -> find ('div.cat') as $elem)
+    		{
+    			$order_info = array(
+    				'name' => $elem -> find('div.text p.namelink a',0) -> plaintext,
+                    'price' => $elem -> find('div.text p.pr strong',0) -> plaintext,
+                    'link' => $elem -> find('a',0) -> href,
+                    'size' => $elem -> find('div.text p[3]',0) -> plaintext,
+    			);		 
+    			$orders_info[] = $order_info;
+    		}
+    		return $orders_info;
+
+    	}
+
+    	foreach (get_info('http://mebel-son.ru/katalog_tovarov/stenki_v_gostinuyu/requestParams/page/2') as $key => $value)
+   		{
+            $kal[] = $value;
+   		}
+   		// fck_soc
+        file_put_contents('test.json', json_encode($kal, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
+        ?>
+    </body>
+</html>
